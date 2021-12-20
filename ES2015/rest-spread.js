2513cd0c306c72a825185a58e78d4cd53ba571c4
Laura <law7676@outlook.com>
2021-12-20 08:16:57 -0600

    completed ES2015/rest-spread.js assignment

--- ES2015/rest-spread.js
+++ ES2015/rest-spread.js
@@ -72,32 +72,31 @@ function extend(array1, array2) {
   return [...array1, ...array2];
 }
 
-console.log(extend([1, 2, 3,], [9, 8, 7]));
-
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
 function addKeyVal(obj, key, val) {
-
+  return { ...obj, [key]: val };
 }
-
 
 /** Return a new object with a key removed. */
 
 function removeKey(obj, key) {
-
+  const newObj = {...obj};
+  delete newObj[key];
+  return newObj;
 }
 
 
 /** Combine two objects and return a new object. */
 
 function combine(obj1, obj2) {
-
+  return {...obj1, ...obj2}
 }
 
 
 /** Return a new object with a modified key and value. */
 
 function update(obj, key, val) {
-
+  return {...obj, [key]: val};
 }
