7e34ccc48dfe67a8cdfc0f480937a5e038fce21f
Laura <law7676@outlook.com>
2022-03-12 09:40:55 -0600

    added tests

--- react-state-carousel-coins/carousel/src/Carousel.test.js
+++ react-state-carousel-coins/carousel/src/Carousel.test.js
@@ -2,6 +2,15 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 
+it('renders without crashing', function(){
+  render(<Carousel />)
+})
+
+it('matches snapshot', function(){
+  const { asFragment } = render(<Carousel />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
 it("works when you click on the right arrow", function() {
   const { queryByTestId, queryByAltText } = render(<Carousel />);
 
@@ -16,4 +25,13 @@ it("works when you click on the right arrow", function() {
   // expect the second image to show, but not the first
   expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+});
+
+it("works when you click on the right arrow", function() {
+  const { queryByAltText } = render(<Carousel />);
+
+ // expect the first image to show, but not the second
+ expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+ expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
 });
