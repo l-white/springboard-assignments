00a23bc1393174f0637a99e99cbe7f851ecc273f
Laura <law7676@outlook.com>
2022-03-20 18:36:41 -0500

    update python-oo-practice

--- python-oo-practice/serial.py
+++ python-oo-practice/serial.py
@@ -20,3 +20,23 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=0):
+        """Make a new generator, starting at start."""
+
+        self.start = self.next = start
+
+    def __repr__(self):
+        """Show representation."""
+
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+
+    def generate(self):
+        """Return next serial."""
+
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        """Reset number to original start."""
+
+        self.next = self.start
